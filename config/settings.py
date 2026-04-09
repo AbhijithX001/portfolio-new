f86db487ab9feed55e5f14935f8fc572ef91ef06
AbhijithX001 <abhijithprakash22334a@gmail.com>
2026-04-09 12:34:23 +0530

    render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yoqx&geah#c8bz7#(%a)js#3y(8qc_3e-=&597z=r8_x=kmdxe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
